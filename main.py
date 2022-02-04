06491d84ef4e3422ad1f574fed565c2f4f328ca2
taminaminam <services@taminaminam.icu>
2022-02-04 23:02:54 +0100

    added cat! command

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import random
 import cat_api
 import os
 
@@ -6,6 +7,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+BOT_OWNER_ID = os.getenv('BOT_OWNER_ID')
 
 client = discord.Client()
 
@@ -14,5 +16,16 @@ async def on_ready():
     print(f'{client.user} has connected to Discord!')
     for guild in client.guilds:
         print(f'{client.user} has connected to the guild: {guild.name} (id: "{guild.id}")')
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    if message.content == 'cat!':
+        response = cat_api.get_url()
+        print(f'User "{message.author}" used command "cat!" in channel "{message.channel}" (Guild: "{message.guild.id}" => "{message.guild}")')
+        await message.channel.send(response)
+    
 
 client.run(TOKEN)
